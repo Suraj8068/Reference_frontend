a122caae8d7afe24d9b256b719bb8bb38f024365
Suraj Singh <surajsingh99241@gmail.com>
2025-03-13 11:02:15 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Auth_front';
+
+  showSidebar = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (this.router.url === '/' || this.router.url === '/login') {
+          this.showSidebar = false; // Login Page pe Sidebar nahi dikhega
+        } else {
+          this.showSidebar = true; // Baaki Sab Pages pe Sidebar dikhega
+        }
+      }
+    });
+  }
 }
